018bf0866ad7f6db681e6ab21b00afe47e6841ee
Sofie Van Dingenen <sofie.van.dingenen@iths.se>
2024-02-08 14:46:24 +0100

    added 2 language to the menu

--- src/main/java/se/ju23/typespeeder/Console.java
+++ src/main/java/se/ju23/typespeeder/Console.java
@@ -2,14 +2,20 @@ package se.ju23.typespeeder;
 
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+
 @Component
 public class Console {
     public void print(String message){
         System.out.println(message);
     }
-    public void print(String[] stringList){
-        for(String s: stringList){
-            print(s);
+    public void print(String[] stringList, HashMap<String, String> languageMap){
+        for(int i = 0; i < stringList.length; i++){
+            print(i+1 + ". " + languageMap.get(stringList[i]));
         }
+    }
+
+    public void print(HashMap<String, String> languageMap, String text) {
+        System.out.println(languageMap.get(text));
     }
 }
--- src/main/java/se/ju23/typespeeder/MenuService.java
+++ src/main/java/se/ju23/typespeeder/MenuService.java
@@ -1,7 +1,11 @@
 package se.ju23.typespeeder;
 
+import org.apache.logging.log4j.util.EnglishEnums;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.WeakHashMap;
 
 /**
  * @author  Sofie Van Dingenen
@@ -13,22 +17,37 @@ public class MenuService {
     @Autowired
     Console console;
 
+    HashMap<String, String> enMap = new HashMap<>();
+    HashMap<String, String> svMap = new HashMap<>();
+
     String[] optionList = {
-            "1. Log in",
-            "2. make account",
-            "3.exit"
+            "menu.option.login",
+            "menu.option.newUser",
+            "menu.option.exit"
     };
 
     public void startMenu(){
-        //TODO: choose language
+        console.print("""
+                --------------------------------------
+                TypeSpeeder
+                
+                welcome/ välkommen 
+                --------------------------------------
+                
+                Choose a language to continue/ Välj ett språk för att fortsätta
+                
+                """);
+
+        HashMap<String, String> languageMap = getLanguage();
+
         boolean running = true;
         console.print("""
                 --------------------------------------------------
-                Welcome to TyppeSpeeder!
                 """);
+        console.print(languageMap, "menu.welcome.typeSpeeder");
         while(running){
-            console.print("Choose one of the following options");
-            console.print(optionList);
+            console.print(languageMap, "menu.option.choose");
+            console.print(optionList, languageMap);
             int chosenInt = ScannerHelper.getInt();
             switch (chosenInt){
                 case 1 -> console.print("you are logged in");
@@ -36,6 +55,38 @@ public class MenuService {
                 case 3 -> running = false;
             }
         }
+    }
+
+    private void createEnMap(){
+        enMap.put("menu.option.login", "Log in");
+        enMap.put("menu.option.newUser", "make an account");
+        enMap.put("menu.option.exit", "Exit");
+        enMap.put("menu.option.choose", "Choose one of the folowing options");
+        enMap.put("menu.welcome.typeSpeeder", "Welcome to TypeSpeeder!");
+    }
+
+    private void createsvMap(){
+        svMap.put("menu.option.login", "Logga in");
+        svMap.put("menu.option.newUser", "Skapa ett konto");
+        svMap.put("menu.option.exit", "Avsluta");
+        svMap.put("menu.option.choose", "Välj en av nedanstående val");
+        svMap.put("menu.welcome.typeSpeeder","Välkommen till TypeSpeeder! ");
+
+    }
+
+    private HashMap<String, String> getLanguage(){
+        HashMap langugeMap = null;
+        console.print("1. English\n2. Svenska");
+        int chosenInt = ScannerHelper.getInt();
+        if( chosenInt == 1){
+            langugeMap = enMap;
+            createEnMap();
+        }
+        if(chosenInt == 2){
+            langugeMap = svMap;
+            createsvMap();
+        }
+        return langugeMap;
     }
 
 
