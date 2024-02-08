884857323d5d54ce172730f90eb334a5b88c0bd0
Depinder Kaur <depinder.kaur@iths.se>
2024-02-08 16:46:20 +0100

    Created an interface TypeOrDifficultyLevel.

---
+++ src/main/java/se/ju23/typespeeder/TypeOrDifficultyLevel.java
@@ -0,0 +1,14 @@
+package se.ju23.typespeeder;
+
+/**
+ * @author Depinder Kaur
+ * @date 2024-02-08
+ * @ersion 1.0
+ * <h2>TypeOrDifficultyLevel</h2>
+ * TypeOrDifficultyLevel is an interface with just one method.
+ * Its purpose is to get the type or the level of difficulty of a typing game.
+ */
+
+public interface TypeOrDifficultyLevel {
+    String getTypeOrDifficultyLevel();
+}
