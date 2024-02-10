6b1e695be80d6251e0156b4633b25793a5536af0
Depinder Kaur <depinder.kaur@iths.se>
2024-02-10 14:53:46 +0100

    Modified methods which updates user password in the ScannerHelper class.

--- src/main/java/se/ju23/typespeeder/ScannerHelper.java
+++ src/main/java/se/ju23/typespeeder/ScannerHelper.java
@@ -6,25 +6,25 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ScannerHelper {
-   private static Console console = new Console();
-   private static Scanner scanner = new Scanner(System.in).useLocale(Locale.FRANCE);
+    private static Console console = new Console();
+    private static Scanner scanner = new Scanner(System.in).useLocale(Locale.FRANCE);
 
-    public static int getInt(){
-         int number = 0;
-         while( number == 0){
-             boolean validNumber = false;
-             while (! validNumber){
-                 try {
-                     console.print("> ");
-                     number = scanner.nextInt();
-                     validNumber = true;
-                 } catch (Exception e){
-                     console.print("Wrong Value, enter a number: ");
-                     scanner.nextLine();
-                 }
-             }
-         }
-         return number;
+    public static int getInt() {
+        int number = 0;
+        while (number == 0) {
+            boolean validNumber = false;
+            while (!validNumber) {
+                try {
+                    console.print("> ");
+                    number = scanner.nextInt();
+                    validNumber = true;
+                } catch (Exception e) {
+                    console.print("Wrong Value, enter a number: ");
+                    scanner.nextLine();
+                }
+            }
+        }
+        return number;
     }
 
     public static String getStringInputForUsernameOrDisplayName() {
@@ -32,34 +32,42 @@ public class ScannerHelper {
 
         do {
             input = scanner.nextLine();
-            if(input.contains(" ") || input.contains(",")) {
+            if (input.contains(" ") || input.contains(",")) {
                 console.t("---Whitespaces or comma NOT allowed---");
                 console.print("Try again: ");
             }
-        } while(input.trim().isEmpty() || input.contains(" ") || input.contains(","));
+        } while (input.trim().isEmpty() || input.contains(" ") || input.contains(","));
 
         return input;
     }
 
     public static String getStringInputForPassword() {
-        String input;
+        scanner.nextLine();
 
-        boolean match;
-        do {
+        console.t("NOTE: A password must contain atleast 1 capital letter, " +
+                "\n1 small letter, 2 digits and be 6 characters long.");
+        console.print("Enter password: ");
+
+        String input = null;
+        boolean match = false;
+
+        while (!match) {
+
             input = scanner.nextLine();
-            match = input.matches("^\\(?=.*\\d{2,}\\)(?=.*[a-z])(?=.*[A-Z]).{6,}$");
-            if(!match) {
-                console.t("Try again!");
+            match = input.matches("^(?=.*\\d{2,})(?=.*[a-z])(?=.*[A-Z]).{6,}$");
+            if (!match) {
+                console.print("Try again: ");
             }
-        } while(!match);
+        }
 
         return input;
+
     }
 
     public static String validateStringInputForLogin() {
         String input;
         do {
-            input = scanner.nextLine().trim();
+            input = scanner.nextLine();
         } while (input.trim().isEmpty());
         return input;
     }
