b68dfb0e822f8ce43e6ea2318f535207894f928d
Sofie Van Dingenen <sofie.van.dingenen@iths.se>
2024-02-12 19:36:20 +0100

    Language Class to easily know which language is used is added. LanguageMenu class is added to choose a language. LoginMenu class is added to login or to create an account.

---
+++ src/main/java/se/ju23/typespeeder/menu/LanguageMenu.java
@@ -0,0 +1,56 @@
+package se.ju23.typespeeder.menu;
+
+import se.ju23.typespeeder.ScannerHelper;
+import se.ju23.typespeeder.consle.Console;
+import se.ju23.typespeeder.consle.Language;
+
+import java.util.ArrayList;
+
+/**
+ * @author Sofie Van Dingenen
+ * @version 1.0.0
+ * Since 2024-02-08
+ *
+ * <h2>LanguageMenu</h2>
+ *
+ * LanguageMenu class has methods to show and choose a language.
+ */
+public class LanguageMenu {
+    private Console console = new Console();
+
+    public void dsiplayMenu() {
+        console.print("""
+                --------------------------------------
+                TypeSpeeder
+                                
+                welcome/ välkommen 
+                --------------------------------------
+                                
+                Choose a language to continue/ Välj ett språk för att fortsätta
+                                
+                """);
+    }
+
+    private ArrayList<String> getMenuOptions() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("English");
+        list.add("Svenska");
+        return list;
+    }
+
+    /**
+     * Returns a language in a list chosen by the user.
+     * @return the language chosen by the user.
+     */
+    public Language setLanguage() {
+        console.print(getMenuOptions());
+        int chosenInt = ScannerHelper.getInt(getMenuOptions().size());
+        if (chosenInt == 1) {
+            return new Language("eng");
+        }
+        if (chosenInt == 2) {
+            return new Language("sv");
+        }
+        return new Language();
+    }
+}
