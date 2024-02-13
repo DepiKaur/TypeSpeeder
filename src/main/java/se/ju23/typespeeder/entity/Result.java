4150cb4689eaba272f9fd131bbff9be6a8df9eaf
Depinder Kaur <depinder.kaur@iths.se>
2024-02-13 16:09:22 +0100

    Slight modifications

--- src/main/java/se/ju23/typespeeder/entity/Result.java
+++ src/main/java/se/ju23/typespeeder/entity/Result.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -22,12 +23,12 @@ public class Result {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
-    @Column(name="player_id")
     @ManyToOne
+    @JoinColumn(name="player_id")
     private Player player;
 
-    @Column(name="game_id")
     @ManyToOne
+    @JoinColumn(name="game_id")
     private Game game;
 
     private int points;
