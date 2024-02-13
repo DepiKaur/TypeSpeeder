c66548b166eabd419fd0c709cf37f5d0f7c3b4ef
Depinder Kaur <depinder.kaur@iths.se>
2024-02-13 16:10:22 +0100

    Created a new record GuessEvaluation to save the number of correct and the number of most correct in order values.

---
+++ src/main/java/se/ju23/typespeeder/service/GuessEvaluation.java
@@ -0,0 +1,9 @@
+package se.ju23.typespeeder.service;
+
+/**
+ * @author Depinder Kaur
+ * @version <h2></h2>
+ * @date 2024-02-12
+ */
+public record GuessEvaluation(int numOfCorrect, int numOfMostCorrectInOrder) {
+}
