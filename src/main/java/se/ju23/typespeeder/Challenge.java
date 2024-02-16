c2756ef3bc3ca2a94016717de373049a0c8c67ff
Depinder Kaur <depinder.kaur@iths.se>
2024-02-16 10:12:37 +0100

    Added new Challenge class to fulfil tests in update 2.

---
+++ src/main/java/se/ju23/typespeeder/Challenge.java
@@ -0,0 +1,17 @@
+package se.ju23.typespeeder;
+
+/**
+ * @author Depinder Kaur
+ * @version <h2></h2>
+ * @date 2024-02-16
+ */
+public class Challenge {
+
+    public void lettersToType() {
+
+    }
+
+    public void startChallenge() {
+
+    }
+}
