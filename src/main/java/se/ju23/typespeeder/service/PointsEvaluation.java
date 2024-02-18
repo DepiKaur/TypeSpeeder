558d58ede68d1bbab0e9637e065bdfc83dc9a78d
Depinder Kaur <depinder.kaur@iths.se>
2024-02-18 22:22:22 +0100

    Created a new record to handle deducted_points.

---
+++ src/main/java/se/ju23/typespeeder/service/PointsEvaluation.java
@@ -0,0 +1,9 @@
+package se.ju23.typespeeder.service;
+
+/**
+ * @author Depinder Kaur
+ * @version <h2></h2>
+ * @date 2024-02-18
+ */
+public record PointsEvaluation(int bonusPoints, int pointsToBeDeducted) {
+}
