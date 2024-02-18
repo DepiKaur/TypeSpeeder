d62f1d6e76841ab95e010922b3a92e425778a10c
Depinder Kaur <depinder.kaur@iths.se>
2024-02-18 22:18:35 +0100

    Renamed record Evaluation to UserInputEvaluation.

--- src/main/java/se/ju23/typespeeder/service/UserInputEvaluation.java
+++ src/main/java/se/ju23/typespeeder/service/UserInputEvaluation.java
@@ -3,9 +3,9 @@ package se.ju23.typespeeder.service;
 /**
  * @author Depinder Kaur
  * @version 0.1.0
- * <h2>Evaluation</h2>
+ * <h2>UserInputEvaluation</h2>
  * <p>
- *     Evaluation is a <i>record</i> to save the following:
+ *     UserInputEvaluation is a <i>record</i> to save the following:
  *     <ul>
  *         <li>number of correct positions</li>
  *         <li>number of most correct in order</li>
@@ -14,5 +14,5 @@ package se.ju23.typespeeder.service;
  * </p>
  * @date 2024-02-12
  */
-public record Evaluation(int numOfCorrect, int numOfMostCorrectInOrder, int totalNumInGivenString) {
+public record UserInputEvaluation(int numOfCorrect, int numOfMostCorrectInOrder, int totalNumInGivenString) {
 }
