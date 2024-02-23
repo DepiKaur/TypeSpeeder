4f136a4817e4598abb5fa9d302889a60afbcc11d
Sofie Van Dingenen <sofie.van.dingenen@iths.se>
2024-02-23 12:29:21 +0100

    changed how to files are read.

--- src/main/java/se/ju23/typespeeder/util/FileUtil.java
+++ src/main/java/se/ju23/typespeeder/util/FileUtil.java
@@ -4,6 +4,7 @@ import se.ju23.typespeeder.consle.Console;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
@@ -32,13 +33,14 @@ public class FileUtil {
         try {
             ArrayList<String> fileInformation = new ArrayList<>();
 
-            File file = new File(fileName);
-            Scanner textFromFile = new Scanner(file);
+            //File file = new File(fileName);
+            InputStream in = ClassLoader.getSystemClassLoader().getResourceAsStream(fileName);
+            Scanner textFromFile = new Scanner(in);
             while (textFromFile.hasNextLine()) {
                 fileInformation.add(textFromFile.nextLine());
             }
             return fileInformation;
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             console.error("File Not found!");
         }
         return null;
@@ -56,7 +58,7 @@ public class FileUtil {
 
     public static HashMap<String, String> readLanguageFile(String lang) {
         HashMap<String, String> languageMap = new HashMap<>();
-        ArrayList<String> languageInfoFromFile = readFile("src/main/resources/" + lang + ".txt");
+        ArrayList<String> languageInfoFromFile = readFile(lang + ".txt");
 
         for (String languageInfo : languageInfoFromFile) {
             String[] languageInfoString = languageInfo.split(" = ");
